8e71b77d49dd3a1ef217278db1c6fdc2e104b89d
Prachiti Vaidya <prachitivaidya232@gmail.com>
2024-05-08 14:12:55 +0530

    Place order component added

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 import { useContext } from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const Cart = () => {
 
   const{cartItems, food_list, removeFromCart,getTotalCartAmount} = useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -47,16 +50,18 @@ const Cart = () => {
                 <p>Subtotal</p>
                 <p>&{getTotalCartAmount()}</p>
               </div>
+              <hr />
               <div className="cart-total-details">
                 <p>Delivery Fee</p>
-                <p>&{2}</p>
+                <p>&{getTotalCartAmount()===0?0:2}</p>
               </div>
+              <hr />
               <div className="cart-total-details">
                 <b>Total</b>
-                <b>${getTotalCartAmount()+2}</b>
+                <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
               </div>
-              <button>PROCEED TO CHECKOUT</button>
-          </div>
+            </div>
+              <button onClick={()=> navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
